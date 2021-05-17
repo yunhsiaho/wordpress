83f672380aa20d46a34dc885a048909c46802399
Samantha <ramakerssamantha1@gmail.com>
2021-05-17 15:45:13 +0200

    updating footer

--- wordpress/wp-content/themes/myTheme/functions.php
+++ wordpress/wp-content/themes/myTheme/functions.php
@@ -5,7 +5,7 @@ function load_css()
         wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all' );
         wp_enqueue_style('bootstrap');
 
-        wp_register_style('main', get_template_directory_uri() . '/css/main.css', array(), false, 'all' );
+        wp_register_style('main', get_template_directory_uri() . '/css/style.css', array(), false, 'all' );
         wp_enqueue_style('main');
     
 }
