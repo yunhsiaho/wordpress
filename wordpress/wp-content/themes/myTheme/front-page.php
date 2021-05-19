15a70f85e330b40bad5a3175ffcc6bb74fe689d0
hsia <yunhsia.ho@gmail.com>
2021-05-19 11:48:00 +0200

    nice banner

--- wordpress/wp-content/themes/myTheme/front-page.php
+++ wordpress/wp-content/themes/myTheme/front-page.php
@@ -1,7 +1,9 @@
 <?php 
     $text = get_field('title_text'); 
+    $text2 = get_field('title_text_2'); 
     $image = get_field('background-image');
     $background = $image['sizes']['2048x2048'];
+    // $decoPic = 
     $menuLink = get_field('check_our_menu');
     $checkMenuText = get_field('check_menu_text');
 ?>
@@ -11,7 +13,9 @@
     <?php get_header(); ?>       
     <div class="banner-text">
         <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
-        <?php echo $text ?>
+        
+        <p class="for-happy-day"><?php echo $text ?></p>
+        <p class="enjoy-happy-meal"><?php echo $text2 ?></p>
         <div class="check-our-menu">
             ―― <a href="<?php echo $menuLink ?>"><?php echo $checkMenuText ?></a>
         </div>
