7f950e77562edbb0ff840d63b7b4975b3b12ca6f
Samantha <ramakerssamantha1@gmail.com>
2021-05-25 14:44:10 +0200

    updatse

--- wordpress/wp-content/themes/myTheme/page-recipes.php
+++ wordpress/wp-content/themes/myTheme/page-recipes.php
@@ -1,14 +1,30 @@
-<?php get_header(); ?>
+<?php get_header(); 
 
+$text = get_field('title_text'); 
+$text2 = get_field('title_text_2'); 
+
+$image = get_field('lounge_restaurant');
+$picture = $image['sizes']['2048x2048'];
+
+$menuLink = get_field('check_our_menu');
+$checkMenuText = get_field('check_menu_text');
+
+?>
+
+<!-- Recipes page -->
+<div class="background-image">
+    
+</div>
 <div class="container">
-<h1><?php the_title(); ?></h1>
 
+<h1><?php the_title(); ?></h1>
 <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
 
-<div><?php the_content(); ?></div>
 
 
 <?php endwhile; endif; ?>
+<img src="<?php echo $picture;?>" class="img-fluid"></img>
+
 </div>
 
 <?php get_footer(); ?>
